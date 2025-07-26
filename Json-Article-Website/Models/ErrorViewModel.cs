a30be4e78222665922597fd380e785e7005cf7a6
ravivish786 <vv5845055@gmail.com>
2025-07-26 10:25:57 +0530

    Add project files.

---
+++ Json-Article-Website/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Json_Article_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

ceada23c79a612ccb562e80df568b256a273f321
ravivish786 <vv5845055@gmail.com>
2025-07-27 19:27:36 +0530

    Refactor login functionality and update UI components
    - Updated `AccountController.cs` to use `LoginModel` for login, improving data encapsulation and adding simple authentication logic.
    - Modified `LoginModel.cs` to replace `Email` with `UserName` and made `ReturnUrl` nullable.
    - Adjusted `Login.cshtml` to reflect model changes, replacing email input with username and simplifying `ReturnUrl` handling.
    - Enhanced `site.css` by adding `line-height` to `.article-content` for better readability.

--- Json-Article-Website/Controllers/AccountController.cs
+++ Json-Article-Website/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Json_Article_Website.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Json_Article_Website.Controllers
 {
@@ -10,23 +11,35 @@ namespace Json_Article_Website.Controllers
         public Task<IActionResult> Login(string returnUrl = null)
         {
             ViewData["ReturnUrl"] = returnUrl;
-            return Task.FromResult<IActionResult>(View());
+            return Task.FromResult<IActionResult>(View(new LoginModel
+            {
+                ReturnUrl = returnUrl,
+            }));
         }
 
         [HttpPost]
         [Route("login")]
-        public Task<IActionResult> Login(string Email, string password, string ReturnUrl = null)
+        public Task<IActionResult> Login(LoginModel model)
         {
             // Here you would typically validate the user credentials
             // For simplicity, we are assuming the login is always successful
             if (ModelState.IsValid)
             {
-                // Set authentication cookie or session here
-                // Redirect to the return URL or default page
-                return Task.FromResult<IActionResult>(Redirect(ReturnUrl ?? "/"));
+                // valiadte from static data like admin:admin
+
+                if (model.UserName == "admin" && model.Password == "admin")
+                {
+                    // Set the user as authenticated (this is just a placeholder, implement your own authentication logic)
+                    HttpContext.Session.SetString("UserName", model.UserName);
+                    HttpContext.Session.SetString("IsAuthenticated", "true");
+                    // Redirect to the return URL or home page
+                    var adminPage = Url.Action("Index", "Admin");
+                    return Task.FromResult<IActionResult>(Redirect(model.ReturnUrl ?? adminPage ?? "/"));
+                }
+                 
             }
-            ViewData["ReturnUrl"] = ReturnUrl;
-            return Task.FromResult<IActionResult>(View());
+             
+            return Task.FromResult<IActionResult>(View(model));
         }
 
 
--- Json-Article-Website/Models/LoginModel.cs
+++ Json-Article-Website/Models/LoginModel.cs
@@ -5,8 +5,8 @@ namespace Json_Article_Website.Models
     public class LoginModel
     {
         [Required]
-        [Display(Name = "Email")]
-        public string Email { get; set; } = string.Empty;
+        [Display(Name = "User Name")]
+        public string UserName { get; set; } = string.Empty;
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
@@ -14,7 +14,7 @@ namespace Json_Article_Website.Models
         [Display(Name = "Remember Me")]
         public bool RememberMe { get; set; }
         [Display(Name = "Return URL")]
-        public string ReturnUrl { get; set; } = string.Empty;
+        public string? ReturnUrl { get; set; } = null;
 
     }
 }
